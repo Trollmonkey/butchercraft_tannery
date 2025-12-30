bf0d97c51ef218a5e701680fa74e74fa5e82367d
Trollmonkey <^Ka8b9V&oSo@>
2025-12-30 12:53:26 -0800

    Implemented Smoking logic and added a logger to track progress.

--- src/main/java/com/trollmonkey/butchercraft_tannery/TanningRackBlockEntity.java
+++ src/main/java/com/trollmonkey/butchercraft_tannery/TanningRackBlockEntity.java
@@ -7,6 +7,7 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
+import com.trollmonkey.butchercraft_tannery.ButchercraftTannery;
 
 public class TanningRackBlockEntity extends BlockEntity {
 
